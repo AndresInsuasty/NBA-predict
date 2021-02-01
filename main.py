e055af11e3c8a1acef4a5622dbf6c3152dbdece5
Sebastian Lopez Restrepo <selopezre@unal.edu.co>
2021-02-01 17:54:35 -0500

    Lineups text

--- main.py
+++ main.py
@@ -47,17 +47,17 @@ if uploaded_file is not None:
 
         salary = st.number_input("Max Salary", 60000)
 
-        n_teams = st.number_input("¿How much teams do you want?", 20)
+        n_teams = st.number_input("¿How much Lineups do you want?", 20)
 
         #Generacion de equipos
-        st.title('NBA Fantasy points TEAMS')
+        st.title('NBA Fantasy points Lineups')
         output_team = get_teams(data_predict,salary,n_teams) 
         #Muestra de equipos
         pts=0
         cont=1
         for key in output_team:
             pts = output_team[key]['FD_PTS'].sum()
-            st.subheader('TEAM '+str(cont))
+            st.subheader('Lineups '+str(cont))
             st.markdown('**_SALARY_** = '+str(round(output_team[key]['FD_SALARY'].sum(),2))+' USD')
             st.markdown('**_FANTASY POINTS_** = '+str(round(pts,2)))
             st.write(output_team[key])
